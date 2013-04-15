12c76fd6e34326f13929b2b4cef103deb1b96407
Niko Santoro <djsantoroniko@yahoo.it>
2013-04-15 17:23:34 +0200

    Rename

---
+++ Sampler/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Sampler
+//
+//  Created by Nico Santoro on 15/04/13.
+//  Copyright (c) 2013 Nico Santoro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
